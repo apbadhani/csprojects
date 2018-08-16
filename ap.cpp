1c671bbfeb05db568fe2b56b7dd3ecd79481fb7a
manu16101999 <42250690+manu16101999@users.noreply.github.com>
2018-08-16 19:51:48 +0530

    Add files via upload

---
+++ ap.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{int a=2, b=3;
+float fvalue=3.1, fvalue1=6.2;
+cout<<"sum of integers a and b"<<a+b<<endl;
+cout<<"difference of integers a and b"<<b-a<<endl;
+cout<<"multiplication of integers a and b"<<a*b<<endl;
+cout<<"division of integers a and b"<<b/a<<endl;
+cout<<"remainder of integers a and b"<<b%a<<endl;
+cout<<"sum of integers fvalue and fvalue1 "<<fvalue+fvalue1<<endl;
+cout<<"difference of integers fvalue and fvalue1"<<fvalue1-fvalue<<endl;
+cout<<"multiplication of integers fvalue and fvalue1"<<fvalue*fvalue1<<endl;
+cout<<"division of integers fvalue and fvalue1"<<fvalue1/fvalue<<endl;
+return 0;
+}
+
---
+++ apo.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{int a;
+float fvalue;
+double dvalue;
+char name;
+cout<<"the size of int data type is : "<<sizeof(int)<<endl<<"the size of float data type is: "<<sizeof(float)<<endl<<"the size of double data type is: "<<sizeof(double)endl<<"the size of character data type is: "<<sizeof(char)<<endl<<"the size of boolean data type is: "<<sizeof(bool);
+return 0;
+}
